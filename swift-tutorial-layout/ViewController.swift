b7be384f2b5baad93dfd3dd303fa11a9b4d9672f
GuilhermeAFonseca <guilherme.araujo.f@gmail.com>
2023-07-16 22:34:37 -0300

    Criação do projeto. Inclusão do table view de viagens

--- swift-tutorial-layout/ViewController.swift
+++ swift-tutorial-layout/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var viagensTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "viagem \(indexPath.row)"
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viagensTableView.dataSource = self
     }
-
-
 }
-
