09225f912db22abc6ff702466754a9952a5e6a4c
GuilhermeAFonseca <guilherme.araujo.f@gmail.com>
2023-07-24 19:41:54 -0300

    Registrada a celula na tableview

--- swift-tutorial-layout/View/ViewController.swift
+++ swift-tutorial-layout/View/ViewController.swift
@@ -16,17 +16,22 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
-        cell.textLabel?.text = "viagem \(indexPath.row)"
-        return cell
+        guard let celulaViagem = tableView.dequeueReusableCell(withIdentifier: "ViagemTableViewCell") as? ViagemTableViewCell else {
+            fatalError("error to create ViagemTableViewCell")
+        }
+        return celulaViagem
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configuraTableView()
+        view.backgroundColor = UIColor(red: 30, green: 59, blue: 119, alpha: 1)
+    }
+    
+    func configuraTableView(){
+        viagensTableView.register(UINib(nibName: "ViagemTableViewCell", bundle: nil), forCellReuseIdentifier: "ViagemTableViewCell")
         viagensTableView.dataSource = self
         viagensTableView.delegate = self
-        
-        view.backgroundColor = UIColor(red: 30, green: 59, blue: 119, alpha: 1)
     }
 }
 
@@ -40,5 +45,9 @@ extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 300
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
     }
 }
