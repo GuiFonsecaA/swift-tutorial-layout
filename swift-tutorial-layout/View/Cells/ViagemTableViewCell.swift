31a3e6dcc619ee81ccbe4d28f0485d0f90dae2dc
GuilhermeAFonseca <guilherme.araujo.f@gmail.com>
2023-07-24 19:19:35 -0300

    Criação de celula do projeto

---
+++ swift-tutorial-layout/View/Cells/ViagemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViagemTableViewCell.swift
+//  swift-tutorial-layout
+//
+//  Created by Guilherme Fonseca on 24/07/23.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
